a2e07db8d41e2f97808db4f55a6ab9516a0c11f9
Julee <julee.gunjal@joshsoftware.com>
2023-01-11 10:38:45 +0530

    logic for update leaves by admin

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -40,9 +40,16 @@ class LeavesController < ApplicationController
   def update
     @leave = Leave.find(params[:id])
     binding.pry
-    if authorised_admin? || authorised_employee? && @leave.update(leave_params)
-      flash[:notice] = "Sucessfully, updated the status of leave application"
-      redirect_to '/leaves'
+   @inc = skip_weekends
+    paid?
+    if authorised_admin? || authorised_employee?
+      if paid? && @leave.update(leave_params)
+        flash[:notice] = "Sucessfully, updated the status of leave application"
+        redirect_to '/leaves'
+      else 
+        flash[:notice] = "change the type of leaves"
+        redirect_to '/leaves'
+      end
     else
       flash[:notice] = "Unauthorised user "
       redirect_to '/leaves/edit'
@@ -60,12 +67,50 @@ class LeavesController < ApplicationController
     end
   end
 
+
+
   protected
 
   def authorised_employee?
     current_user == Employee.find(@leave.employee_id)
   end
 
+  def paid?
+    @leave = fetch_leave
+    count = Employee.find(@leave.employee_id).paid_leaves
+    @inc = skip_weekends
+    if count < @inc
+      @unpaid = @inc - count
+      false
+    end
+    true
+  end
+   
+   
+  def skip_weekends
+    @leave = fetch_leave
+    date = @leave.from_date
+    @inc = 0
+    until date <= @leave.to_date do
+      if !date.on_weekend?
+        @inc = inc + 1
+      end
+      date = date.next
+    end
+    @inc
+  end
+
+  def update_paid_leaves
+    @leave = fetch_leave
+    @employee = Employee.find(@leave.employee_id)
+    @employee.paid_leaves
+    @employee.update(paid_leaves)
+
+  end
+
+  def fetch_leave
+    @leave = Leave.find(params[:id]) 
+  end
   
   def leave_params
     params.require(:leave).permit(:status, :reason, :from_date, :to_date, :leave_type, :employee_id)
