27d5d4c2b329d5eab39b19ad6af99af4411ac442
Julee <julee.gunjal@joshsoftware.com>
2023-01-11 10:40:21 +0530

    Association in attendance and employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,5 @@ class Employee < User
 
   validates :first_name, presence: true
   has_many :leaves, dependent: :destroy, class_name: "Leave"
-  
+  has_many :attendances, dependent: :destroy, class_name: "Attendance"
 end
