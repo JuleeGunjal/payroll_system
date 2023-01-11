2721accf3e5ef533763570da7ccf94ad31f7e286
Julee <julee.gunjal@joshsoftware.com>
2023-01-11 10:41:10 +0530

    Attendance controller with supportive files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_055315) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_053610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "month"
+    t.integer "working_days"
+    t.string "unpaid_leaves"
+    t.bigint "employee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_attendances_on_employee_id"
+  end
 
   create_table "leaves", force: :cascade do |t|
     t.string "status"
@@ -41,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_055315) do
     t.date "date_of_joining"
     t.text "address"
     t.string "type"
+    t.integer "paid_leaves", default: 4
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
